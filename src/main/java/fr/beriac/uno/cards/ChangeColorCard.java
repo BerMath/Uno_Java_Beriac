39c9e80fb57a81982bebba67ca119a31a8e6c2ac
Beriac Mathieu <mathieu.beriac@ynov.com>
2025-03-22 10:45:21 +0100

    feat : Add ChangeColorCard class

--- src/main/java/fr/beriac/uno/cards/ChangeColorCard.java
+++ src/main/java/fr/beriac/uno/cards/ChangeColorCard.java
@@ -3,54 +3,28 @@ package fr.beriac.uno.cards;
 import java.awt.*;
 
 public class ChangeColorCard extends Card implements PoweredCard {
-    private Color chosenColor;
+    private java.awt.Color chosenColor;
 
-    public ChangeColorCard() {
-        this.chosenColor = null;
+    public java.awt.Color getChosenColor() {
+        return chosenColor;
     }
 
-    public void SetChosenColor(Color chosenColor) {
-        this.chosenColor = chosenColor;
-    }
-
-    public Color getChosenColor() {
-        return this.chosenColor;
+    public void  setChosenColor(java.awt.Color color) {
+        this.chosenColor = color;
     }
 
     @Override
-    public void performAction(Game game) {
-        game.showColorChooser(this);
+    public void performAction(fr.ynov.java.uno.Game game) {
+        this.chosenColor = game.getCurrentPlyer().chosenColor();
     }
 
     @Override
-    public boolean canBePlacedOn(Card card) {
-        return true; // Can be placed on any card
-    }
-
-    @Override
-    public void render(Graphics g, int x, int y, int width, int height) {
-        if (chosenColor != null) {
-            g.setColor(chosenColor);
-            g.fillRoundRect(x, y, width, height, 10, 10);
-        } else {
-            // Draw four-colored card
-            g.setColor(Color.RED);
-            g.fillRoundRect(x, y, width / 2, height / 2, 10, 10);
-            g.setColor(Color.BLUE);
-            g.fillRoundRect(x + width / 2, y, width / 2, height / 2, 10, 10);
-            g.setColor(Color.GREEN);
-            g.fillRoundRect(x, y + height / 2, width / 2, height / 2, 10, 10);
-            g.setColor(Color.YELLOW);
-            g.fillRoundRect(x + width / 2, y + height / 2, width / 2, height / 2, 10, 10);
-        }
-        g.setColor(Color.WHITE);
-        g.drawRoundRect(x, y, width, height, 10, 10);
-        g.setFont(new Font("Arial", Font.BOLD, 12));
-        g.drawString("WILD", x + width / 2 - 20, y + height / 2 + 5);
+    public boolean canBePlayedOn(Card card) {
+            return true;
     }
 
     @Override
     public String toString() {
-        return "Wild";
+        return "WILD)";
     }
 }
