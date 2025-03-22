737c217b78b87f13e9e70b74f91aebd53358031e
Beriac Mathieu <mathieu.beriac@ynov.com>
2025-03-22 10:17:25 +0100

    feat : Add ColoredCard class

--- src/main/java/fr/beriac/uno/cards/ColoredCard.java
+++ src/main/java/fr/beriac/uno/cards/ColoredCard.java
@@ -5,5 +5,32 @@ import java.awt.*;
 public abstract class ColoredCard extends Card {
     private Color color;
 
-    public ColoredCard(Color color) {this.color = color;}
+    public ColoredCard(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    @Override
+    public boolean  canBePlacedOn(Card card) {
+        if (card instanceof ColoredCard) {
+            return ((ColoredCard) card).getColor().equals(this.color);
+        }
+        return false;
+
+    }
+
+    protected String getColorName() {
+        if (color.equals(Color.RED)) return "RED";
+        if (color.equals(Color.GREEN)) return "GREEN";
+        if (color.equals(Color.BLUE)) return "BLUE";
+        if (color.equals(Color.YELLOW)) return "YELLOW";
+        return "UNKNOWN";
+    }
 }
