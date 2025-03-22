4b18e7e2c7b80588dc99fd426765dbfcb1334b30
Beriac Mathieu <mathieu.beriac@ynov.com>
2025-03-22 10:41:48 +0100

    feat : Add PlusCard class

--- src/main/java/fr/beriac/uno/cards/PlusCard.java
+++ src/main/java/fr/beriac/uno/cards/PlusCard.java
@@ -1,13 +1,35 @@
 package fr.beriac.uno.cards;
 
+import fr.ynov.java.uno.Game;
+
 import java.awt.*;
 
-public class PlusCard extends ColoredCard implements  PoweredCard {
-    int power = 2;
+public class PlusCard extends ColoredCard implements PoweredCard {
+    private int power = 2;
 
-    public PlusCard(Color color) {super(color);}
+    public PlusCard(Color color) {
+        super(color);
+    }
+
+    public int getPower() {
+        return power;
+    }
 
     @Override
-    public void performAction() {}
+    public void performAction(Game game) {
+        game.makeNextPlayerDraw(power);
+    }
 
+    @Override
+    public boolean canBePlayedOn(Card card) {
+        if (card instanceof PlusCard) {
+            return true;
+        }
+        return super.canBePlacedOn(card);
+    }
+
+    @Override
+    public String toString() {
+        return getColorName() + " +2";
+    }
 }
