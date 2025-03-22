ef29b2466301fe85ccc2b59c8cf2068c032b457d
Beriac Mathieu <mathieu.beriac@ynov.com>
2025-03-22 10:23:13 +0100

    feat : Add NormalCard class

--- src/main/java/fr/beriac/uno/cards/NormalCard.java
+++ src/main/java/fr/beriac/uno/cards/NormalCard.java
@@ -3,10 +3,27 @@ package fr.beriac.uno.cards;
 import java.awt.*;
 
 public class NormalCard extends ColoredCard {
-    int number;
+    private int number;
 
-    public NormalCard(Color color, int number) {
+    public NormalCard(Color  color, int number) {
         super(color);
         this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean canBePlayedOn(Card card) {
+        if (card instanceof NormalCard && ((NormalCard) card).getNumber() == this.number) {
+            return true;
+        }
+        return super.canBePlacedOn(card);
+    }
+
+    @Override
+    public String toString() {
+        return getColor() + " " + getNumber();
     }
 }
