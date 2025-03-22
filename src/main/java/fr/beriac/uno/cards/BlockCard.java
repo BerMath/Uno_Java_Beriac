a7f8e7938bddbc642b4eeb14479085d6275b2247
Beriac Mathieu <mathieu.beriac@ynov.com>
2025-03-22 10:33:27 +0100

    feat : Add BlockCard class

--- src/main/java/fr/beriac/uno/cards/BlockCard.java
+++ src/main/java/fr/beriac/uno/cards/BlockCard.java
@@ -1,12 +1,30 @@
 package fr.beriac.uno.cards;
 
+
+import fr.ynov.java.uno.Game;
+
 import java.awt.*;
 
 public class BlockCard extends ColoredCard implements PoweredCard {
-    public BlockCard(Color color) {super(color);}
+    public BlockCard(Color color) {
+        super(color);
+    }
 
     @Override
-    public void performAction() {
+    public void perforAction(Game game) {
+        game.skipNextPlayer();
+    }
 
+    @Override
+    public boolean canBePlayedOn(Card card) {
+        if (card instanceof BlockCard) {
+            return true;
+        }
+        return super.canBePlacedOn(card);
+    }
+
+    @Override
+    public String toString() {
+        return getColorName() + " BLOCK";
     }
 }
