dd3c1b4d9bcb796d12cf261f4d00936ae97f4474
Beriac Mathieu <mathieu.beriac@ynov.com>
2025-03-22 10:37:16 +0100

    feat : Add SwitchCard class

--- src/main/java/fr/beriac/uno/cards/SwitchCard.java
+++ src/main/java/fr/beriac/uno/cards/SwitchCard.java
@@ -2,11 +2,26 @@ package fr.beriac.uno.cards;
 
 import java.awt.*;
 
-public class SwitchCard extends ColoredCard implements PoweredCard {
-    public SwitchCard(Color color) {super(color);}
+public class SwtichCard extends ColoredCard implements PoweredCard {
+    public SwtichCard(Color color) {
+        super(color);
+    }
 
     @Override
-    public void performAction() {
+    public void performAction(fr.ynov.java.uno.Game game){
+        game.reverseDirection();
+    }
 
+    @Override
+    public boolean canBePlayedOn(Card card) {
+        if (card instanceof SwtichCard) {
+            return true;
+        }
+        return super.canBePlacedOn(card);
+    }
+
+    @Override
+    public String toString() {
+        return getColorName() + " SWITCH";
     }
 }
